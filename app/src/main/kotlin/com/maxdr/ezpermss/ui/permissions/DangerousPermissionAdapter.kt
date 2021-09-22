746ae620dc2d5b54158567244c0401b5f54d0236
XamDR <maxdr.mat@gmail.com>
2021-09-22 12:51:34 -0500

    Added option to show the full summary of a dangerous permission in an alert dialog.

--- app/src/main/kotlin/com/maxdr/ezpermss/ui/permissions/DangerousPermissionAdapter.kt
+++ app/src/main/kotlin/com/maxdr/ezpermss/ui/permissions/DangerousPermissionAdapter.kt
@@ -1,8 +1,12 @@
 package com.maxdr.ezpermss.ui.permissions
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.maxdr.ezpermss.R
 import com.maxdr.ezpermss.core.DangerousPermissionInfo
 import com.maxdr.ezpermss.databinding.DangerousPermissionRowLayoutBinding
 
@@ -22,7 +26,9 @@ class DangerousPermissionAdapter(private val dangerousPermissions: List<Dangerou
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DangerousPermissionViewHolder {
 		val binding = DangerousPermissionRowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-		return DangerousPermissionViewHolder(binding)
+		return DangerousPermissionViewHolder(binding).apply {
+			binding.moreOptions.setOnClickListener { buildPopupMenu(it, adapterPosition) }
+		}
 	}
 
 	override fun onBindViewHolder(holder: DangerousPermissionViewHolder, position: Int) {
@@ -31,4 +37,31 @@ class DangerousPermissionAdapter(private val dangerousPermissions: List<Dangerou
 	}
 
 	override fun getItemCount() = dangerousPermissions.size
+
+	private fun buildPopupMenu(view: View, position: Int) {
+		PopupMenu(view.context, view).apply {
+			inflate(R.menu.dangerous_permission_item_context_menu)
+			setOnMenuItemClickListener { item ->
+				when (item.itemId) {
+					R.id.more_info_summary -> {
+						showSummaryInfo(view, position); true
+					}
+					R.id.set_schedule -> {
+						android.util.Log.d("ITEM", position.toString()); true
+					}
+					else -> false
+				}
+			}
+			show()
+		}
+	}
+
+	private fun showSummaryInfo(view: View, position: Int) {
+		val dangerousPermission = dangerousPermissions[position]
+		MaterialAlertDialogBuilder(view.context).apply {
+			setTitle(dangerousPermission.name)
+			setMessage(dangerousPermission.summary)
+			setPositiveButton(R.string.ok) { dialog, _ -> dialog.dismiss() }
+		}.show()
+	}
 }
